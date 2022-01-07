dc1e469c1ee8032562eb2dc4b74fcbae96c34fbf
Georgi Dimitrov <georgedimitrov92@gmail.com>
2022-01-07 16:41:02 +0200

    added comments and default switch case

--- users.sh
+++ users.sh
@@ -1,29 +1,43 @@
+# create and infinite loop, so the user always gets the menu
 while :
 do
+# print out menu options
 echo "-----------------------------------"
 echo "Select user listing option"
 echo "1. List all users"
 echo "2. List users filtered by their username"
 echo "3. List users filtered by their user id"
-echo "4. Exit from string"
+echo "4. Exit from script"
 echo "-----------------------------------"
 
 echo -n "Enter your menu choice [1-4]: "
-
+# read user input
 read menuChoice
 
+# switch case with each action
 case $menuChoice in 
-    1)  getent passwd | cut -d: -f1;;
+    # just list all users
+    1)  getent passwd
+        # end of block
+        ;;
     2)  echo "Write string to filter by:"
+        # read namefilter from input
         read nameFilter
+        # filter out users output with the nameFilter
         getent passwd | grep $nameFilter;;
-    3)  echo "write user id to filter by:"
+    3)  echo "Write user id to filter by:"
+        # read userIdFilter from input
         read userIdFilter
-        # getent passwd | grep :$userIdFilter
+        # use awk with the users database by separating the cols by separator :
+        # condition to output (is userIdFilter equal to the third col)
         awk -v u=$userIdFilter -F":" '$3 == u' /etc/passwd
         ;;
     4)  echo "bye ;)"
+        # exit from script
         exit;;
+    *)  # just catch all invalid menu option
+        echo "invalid input"
+        ;;
     
 esac
 done
