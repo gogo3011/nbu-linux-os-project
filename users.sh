44094be8cb1246e9ddba8f4b4107e3c26e8d0243
Georgi Dimitrov <georgedimitrov92@gmail.com>
2023-10-07 23:12:37 +0300

    Progress upload

--- users.sh
+++ users.sh
@@ -1,12 +1,13 @@
 # create and infinite loop, so the user always gets the menu
+# and can execute a function
 while :
 do
 # print out menu options
 echo "-----------------------------------"
 echo "Select user listing option"
 echo "1. List all users"
-echo "2. List users filtered by their username"
-echo "3. List users filtered by their user id"
+echo "2. List users filtered by a regex pattern"
+echo "3. List users filtered by their username"
 echo "4. Exit from script"
 echo "-----------------------------------"
 
@@ -16,21 +17,21 @@ read menuChoice
 
 # switch case with each action
 case $menuChoice in 
-    # just list all users
+        # just list all users
     1)  getent passwd
         # end of block
         ;;
-    2)  echo "Write string to filter by:"
-        # read namefilter from input
-        read nameFilter
-        # filter out users output with the nameFilter
-        getent passwd | grep $nameFilter;;
-    3)  echo "Write user id to filter by:"
+    2)  echo "Write string/regex to filter by:"
+        # read filter from input
+        read filter
+        # filter out users output with the filter
+        getent passwd | grep $filter;;
+    3)  echo "Write username chars to filter by:"
         # read userIdFilter from input
-        read userIdFilter
+        read userNameFilter
         # use awk with the users database by separating the cols by separator :
         # condition to output (is userIdFilter equal to the third col)
-        awk -v u=$userIdFilter -F":" '$3 == u' /etc/passwd
+        getent passwd | awk -v u="$userNameFilter" : 'index($1, u)'
         ;;
     4)  echo "bye ;)"
         # exit from script
